fbb0281b23cffc0b67040bba9e0194b281d0df6b
zhouwenkai <zhouwenkai@baidu.com>
2018-06-13 23:16:10 +0800

    Review code

--- sample/src/main/java/com/kevin/delegationadapter/sample/pro/meishijie/adapter/MeishiSancanAdapterDelegate.java
+++ sample/src/main/java/com/kevin/delegationadapter/sample/pro/meishijie/adapter/MeishiSancanAdapterDelegate.java
@@ -22,7 +22,7 @@ import java.util.List;
 /**
  * MeishiSancanAdapterDelegate
  *
- * @author zhouwenkai@baidu.com, Created on 2018-06-13 12:34:00
+ * @author zwenkai@foxmail.com, Created on 2018-06-13 12:34:00
  *         Major Function：<b></b>
  *         <p/>
  *         注:如果您修改了本类请填写以下内容作为记录，如非本人操作劳烦通知，谢谢！！！
--- sample/src/main/java/com/kevin/delegationadapter/sample/pro/meishijie/bean/Meishi.java
+++ sample/src/main/java/com/kevin/delegationadapter/sample/pro/meishijie/bean/Meishi.java
@@ -5,7 +5,7 @@ import java.util.List;
 /**
  * Meishi
  *
- * @author zhouwenkai@baidu.com, Created on 2018-06-13 11:02:21
+ * @author zwenkai@foxmail.com, Created on 2018-06-13 11:02:21
  *         Major Function：<b></b>
  *         <p/>
  *         注:如果您修改了本类请填写以下内容作为记录，如非本人操作劳烦通知，谢谢！！！
