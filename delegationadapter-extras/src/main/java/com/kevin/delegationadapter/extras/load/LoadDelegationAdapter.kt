288d7dae4128b6fecb2e006a8290658e2a03a415
zhouwenkai <zhouwenkai@baidu.com>
2019-03-09 21:26:16 +0800

    Review code

--- delegationadapter-extras/src/main/java/com/kevin/delegationadapter/extras/load/LoadDelegationAdapter.kt
+++ delegationadapter-extras/src/main/java/com/kevin/delegationadapter/extras/load/LoadDelegationAdapter.kt
@@ -1,5 +1,17 @@
 /*
- * Copyright (C) 2019 Baidu, Inc. All Rights Reserved.
+ * Copyright (c) 2019 Kevin zhou
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.kevin.delegationadapter.extras.load
 
@@ -12,7 +24,7 @@ import com.kevin.delegationadapter.extras.span.SpanDelegationAdapter
 /**
  * LoadMoreDelegationAdapter
  *
- * @author zhouwenkai@baidu.com, Created on 2019-03-08 23:39:15
+ * @author zwenkai@foxmail.com, Created on 2019-03-08 23:39:15
  *         Major Function：<b>DelegationAdapter with load more</b>
  *         <p/>
  *         Note: If you modify this class please fill in the following content as a record.
--- sample/src/main/java/com/kevin/delegationadapter/sample/multitype/chat/common/ChatLoadAdapterDelegate.java
+++ sample/src/main/java/com/kevin/delegationadapter/sample/multitype/chat/common/ChatLoadAdapterDelegate.java
@@ -1,14 +1,24 @@
 /*
- * Copyright (C) 2019 Baidu, Inc. All Rights Reserved.
+ * Copyright (c) 2019 Kevin zhou
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.kevin.delegationadapter.sample.multitype.chat.common;
 
-import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.kevin.delegationadapter.extras.load.LoadAdapterDelegate;
 import com.kevin.delegationadapter.extras.load.LoadDelegationAdapter;
@@ -18,7 +28,7 @@ import com.kevin.delegationadapter.sample.R;
 /**
  * ChatLoadAdapterDelegate
  *
- * @author zhouwenkai@baidu.com, Created on 2019-03-09 12:55:19
+ * @author zwenkai@foxmail.com, Created on 2019-03-09 12:55:19
  * Major Function：<b></b>
  * <p/>
  * Note: If you modify this class please fill in the following content as a record.
