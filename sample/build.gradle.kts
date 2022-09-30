a06282f22aa9a2e4b8620310177e88e9ba29d4d8
Kevin.zhou <zwenkai@foxmail.com>
2022-09-30 13:54:18 +0800

    Migrating build logic from Groovy to Kotlin

---
+++ sample/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "com.kevin.delegationadapter.sample"
+        minSdk = 14
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    buildFeatures {
+        dataBinding = true
+    }
+
+}
+
+dependencies {
+    implementation(fileTree(baseDir = "libs") {
+        include("*.jar", "*.aar")
+    })
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test:runner:1.4.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.code.gson:gson:2.8.9")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.zwenkai:loopview:2.0.1")
+//    implementation project(':delegationadapter')
+//    implementation project(':delegationadapter-extras')
+    implementation("com.zwenkai:delegationadapter:2.0.8")
+    implementation("com.zwenkai:delegationadapter-extras:2.0.8")
+    implementation("com.scwang.smartrefresh:SmartRefreshLayout:1.1.0-alpha-6")
+    implementation("androidx.viewpager:viewpager:1.0.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+}
